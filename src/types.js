226d7a3f5f08a0c9ee13a31afb4f130d95bfd8a3
aarontxz <115057223+aarontxz@users.noreply.github.com>
2024-02-18 06:10:50 +0800

    add type.js

---
+++ src/types.js
@@ -0,0 +1,9 @@
+export class Conversation {
+  constructor(id, prompts, responses) {
+    this.id = id;
+    this.prompts = prompts;
+    this.responses = responses;
+  }
+}
+
+export default Conversation;
